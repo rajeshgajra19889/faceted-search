4a982168a380d79448f85cc572d262eb3cd346d0
slava pocheptsov <slava pocheptsov@localhost>
2011-03-06 03:14:25 +0200

    Example of Machine Specification test, assembly properties

---
+++ src/FacetedSearch.Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+using It= Machine.Specifications.It;
+
+namespace FacetedSearch.Tests
+{
+    [Subject("Recent Account Activity Summary page")]
+    public class when_a_customer_first_views_the_account_summary_page
+    {
+        It should_display_all_account_transactions_for_the_past_thirty_days;
+        It should_display_debit_amounts_in_red_text;
+        It should_display_deposit_amounts_in_black_text;
+    }
+}
--- src/FacetedSearch.Tests/Properties/AssemblyInfo.cs
+++ src/FacetedSearch.Tests/Properties/AssemblyInfo.cs
@@ -8,10 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FacetedSearch.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FacetedSearch.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -31,5 +27,3 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
