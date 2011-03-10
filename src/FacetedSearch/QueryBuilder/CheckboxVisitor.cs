2daed93d0975aa66c7bcfff915fe5731f7176e2e
slava pocheptsov <slava pocheptsov@localhost>
2011-03-10 16:47:49 +0200

    Checkbox visitor

---
+++ src/FacetedSearch/QueryBuilder/CheckboxVisitor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FacetedSearch.Common;
+using FacetedSearch.Params;
+
+namespace FacetedSearch.QueryBuilder
+{
+    public class CheckboxVisitor : IVisitor<CheckboxSearchOptionsParam>
+    {
+        public object Visit(CheckboxSearchOptionsParam element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit<T>(T element) where T : class, ISearchOptionsParam
+        {
+            var el = element as CheckboxSearchOptionsParam;
+            Visit(el);
+        }
+    }
+}
