5406008ff98c225af1db05c549153f1cd2a34e46
slava pocheptsov <slava pocheptsov@localhost>
2011-03-16 12:14:46 +0200

    Mvc Html helper, test home controller, view, viewmodel

---
+++ src/FacetedSearch.Web/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FacetedSearch.Web.Models;
+
+namespace FacetedSearch.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            var viewModel = new HomeViewModel();
+            return View(viewModel);
+        }
+
+    }
+}
