969cf1808393a1ebb91bdf46dd1aa56a71f82d0e
slava pocheptsov <slava pocheptsov@localhost>
2011-03-21 20:03:34 +0200

    Typed HtmlTagBuilder

---
+++ src/FacetedSearch/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.UI;
+
+namespace FacetedSearch.Extensions
+{
+    public static class IDictionaryExtensions
+    {
+        public static void Add(this IDictionary<string, object> dictionary, HtmlTextWriterAttribute attribute,
+                               object value)
+        {
+            dictionary.Add(attribute.ToString(), value);
+        }
+    }
+}
---
+++ src/FacetedSearch/Extensions/TagBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace FacetedSearch.Extensions
+{
+    public static class TagBuilderExtensions
+    {
+        public static void MergeAttribute(this TagBuilder tagBuilder, HtmlTextWriterAttribute attribute, object value,
+                                          bool replaceExisting = false)
+        {
+            tagBuilder.MergeAttribute(attribute.ToString(), value.ToString(), replaceExisting);
+        }
+        public static void MergeAttributes<TKey, TValue>(this TagBuilder tagBuilder, IDictionary<TKey, TValue> attributes,
+                                          bool replaceExisting = false)
+        {
+            tagBuilder.MergeAttributes(attributes, replaceExisting);
+        }
+    }
+}
